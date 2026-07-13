238de355281b11b0a1f88ea22b87563eef6565d4
keris terto <kterto@gmail.com>
2026-07-13 11:35:22 +0200

    Primer commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,18 @@
+describe ("Login Test Suite", () => {
+    it("Login com sucesso", () => {
+        cy.request({
+            method: "POST", 
+            url:"https://restful-booker.herokuapp.com/auth", 
+            body: {
+                username: "admin",
+                password: "password123"
+            }
+        
+        
+        }).then((resultado) => {
+            expect(resultado.status).to.eq(200);
+            expect(resultado.body).to.have.property("token");
+        })
+
+     })
+    });
