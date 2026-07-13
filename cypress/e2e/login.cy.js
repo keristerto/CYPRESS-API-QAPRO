1f40f23b07c49afe577698d5fc7fc29db9b3c0f1
keris terto <kterto@gmail.com>
2026-07-13 12:18:29 +0200

    testes para login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,7 +12,71 @@ describe ("Login Test Suite", () => {
         }).then((resultado) => {
             expect(resultado.status).to.eq(200);
             expect(resultado.body.token).to.not.be.empty;
-        })
-
-     })
+        });
     });
+
+    it("Login com falha", () => {
+        cy.request({
+            method: "POST", 
+            url:"https://restful-booker.herokuapp.com/auth", 
+            body: {
+                username: "admin",
+                password: "wrongpassword"
+            }
+        
+        
+        }).then((resultado) => {
+            expect(resultado.status).to.eq(200);
+            expect(resultado.body.reason).to.eq("Bad credentials");
+        });
+    });
+
+     it("Login com usuário inválido", () => {
+        cy.request({
+            method: "POST", 
+            url:"https://restful-booker.herokuapp.com/auth", 
+            body: {
+                username: "invalidUser",
+                password: "password123"
+            }
+        
+        
+        }).then((resultado) => {
+            expect(resultado.status).to.eq(200);
+            expect(resultado.body.reason).to.eq("Bad credentials");
+        });
+    });
+
+     it("Login com usuário inválido e senha inválida", () => {
+        cy.request({
+            method: "POST", 
+            url:"https://restful-booker.herokuapp.com/auth", 
+            body: {
+                username: "invalidUser",
+                password: "wrongpassword"
+            }
+        
+        
+        }).then((resultado) => {
+            expect(resultado.status).to.eq(200);
+            expect(resultado.body.reason).to.eq("Bad credentials");
+        });
+    });
+
+   it("Login com usuário inválido e senha vazios", () => {
+        cy.request({
+            method: "POST", 
+            url:"https://restful-booker.herokuapp.com/auth", 
+            body: {
+                username: "",
+                password: ""
+            }
+        
+        
+        }).then((resultado) => {
+            expect(resultado.status).to.eq(200);
+            expect(resultado.body.reason).to.eq("Bad credentials");
+        });
+    });
+
+});
